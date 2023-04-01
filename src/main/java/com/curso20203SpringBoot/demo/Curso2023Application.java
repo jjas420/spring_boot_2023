e95ed54b3832007a2565631a3beb4bca9426fcc9
jjas420 <ayosuajon4@gmail.com>
2023-04-01 15:44:59 -0300

    clase_01_04_2023

---
+++ src/main/java/com/curso20203SpringBoot/demo/Curso2023Application.java
@@ -0,0 +1,14 @@
+package com.curso20203SpringBoot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Curso2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Curso2023Application.class, args);
+                System.out.println("hola");
+	}
+
+}
---
+++ src/test/java/com/curso20203SpringBoot/demo/Curso2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso20203SpringBoot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Curso2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
